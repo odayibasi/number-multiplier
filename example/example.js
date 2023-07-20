df9add5ac6743e820c968ccd8c1a13be4b92a963
odayibasi <odayibasi@gmail.com>
2023-07-20 15:59:23 +0300

    New Version added to NPM

--- example/example.js
+++ example/example.js
@@ -1,7 +1,7 @@
 // example/example.js
 import React from "react";
 import ReactDOM from "react-dom";
-import NumberMultiplier from "../NumberMultiplier";
+import NumberMultiplier from "../src/NumberMultiplier";
 
 ReactDOM.render(
   <NumberMultiplier multiplier={5} />,
